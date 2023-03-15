3a9b56c6d7deebaec8999415ecde2d7f7241936f
Egor Zavrazhnov <88378430+SHREDDING8@users.noreply.github.com>
2023-03-15 14:01:45 +0300

    Initial Commit

---
+++ testAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testAssignment
+//
+//  Created by SHREDDING on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
